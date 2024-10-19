d3749a22a1c3b3e60fee543907879e156d0536c6
SebastianHernandez69 <sh3641046@gmail.com>
2024-10-19 15:44:31 -0600

    microservicio para obtener informacion del usuario(tutor y pupilo)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,37 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { Request } from 'express';
 import { JwtPayload } from 'src/interfaces/JwtPayload';
+import { PreguntaDto } from './dto/pregunta.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('/aggMateria')
+  @Post('/materia-interes/add')
   @UseGuards(JwtAuthGuard)
-  async aggMateriaToUser(@Req() req: Request){
+  async addMateriaToUser(@Req() req: Request){
 
     const user = req.user as JwtPayload;
     return await this.userService.addMateriaToUser(user.sub, req.body.idMateria, user.rol);
+  }
+
+  // Preguntas
+  @Post('/pregunta/add')
+  @UseGuards(JwtAuthGuard)
+  async addPreguntaPupilo(@Req() req: Request, @Body() preguntaDto: PreguntaDto){
+
+    const user = req.user as JwtPayload;
+    return await this.userService.addPreguntaPupilo(user.sub, preguntaDto);
+
+  }
+
+  //Perfil del usuario
+  @Get("/profile")
+  @UseGuards(JwtAuthGuard)
+  async getUserProfile(@Req() req: Request){
+    const user = req.user as JwtPayload;
+    return await this.userService.getUserProfile(user.sub, user.rol);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PreguntaDto } from './dto/pregunta.dto';
+import { format } from 'date-fns';
 
 @Injectable()
 export class UserService {
@@ -39,4 +41,95 @@ export class UserService {
         }
     }
 
+
+    // Preguntas pupilo
+    async addPreguntaPupilo(idUsuario: number,preguntaDto: PreguntaDto){
+
+        const now = new Date();
+
+        try{
+            const pregunta = await this.prismaService.pregunta.create({
+                data: {
+                    idMateria: preguntaDto.idMateria,
+                    idUsuarioPupilo: idUsuario,
+                    titulo: preguntaDto.titulo,
+                    descripcion: preguntaDto.descripcion,
+                    idEstadoPregunta: preguntaDto.idEstadoPregunta,
+                    fechaPublicacion: now
+                }
+            });
+
+            return pregunta;
+        }catch(error){
+            throw new BadRequestException("Error al crear pregunta: "+error);
+        }
+
+    }
+
+    // Perfil usuario
+    async getUserProfile(idUsuario: number, idRol: number){
+        if(idRol === 1){
+            return this.getUserProfileTutor(idUsuario);
+        }
+        else if(idRol === 2){
+            return this.getUserProfilePupilo(idUsuario);
+        }
+        else {
+            throw new BadRequestException("Rol no valido");
+        }
+    }
+
+
+    async getUserProfilePupilo(idUsuario: number){
+        try {
+            const user = await this.prismaService.usuario.findUnique({
+                where: {idUsuario},
+                select: {
+                    nombre: true,
+                    edad: true,
+                    correo: true,
+                    dni: true,
+                    telefono: true,
+                    valoracion: true,
+                    fotoPerfil: true,
+                    horarioDisponibleInicio: true,
+                    horarioDisponibleFin:true,
+                    rol: true,
+                    pregunta: true
+                }
+            });
+    
+            return user;
+        } catch (error) {
+            throw new BadRequestException("Error al obtener perfil pupilo" + error);
+        }
+    }
+
+    async getUserProfileTutor(idUsuario: number){
+        try {
+            const user = await this.prismaService.usuario.findUnique({
+                where: {idUsuario},
+                select: {
+                    nombre: true,
+                    edad: true,
+                    correo: true,
+                    dni: true,
+                    telefono: true,
+                    valoracion: true,
+                    fotoPerfil: true,
+                    horarioDisponibleInicio: true,
+                    horarioDisponibleFin:true,
+                    rol: true,
+                    experiencia: true,
+                    conocimiento: true
+                }
+            });
+    
+            return user;
+        } catch (error) {
+            throw new BadRequestException("Error a obtener perfil tutor: " + error);
+        }
+    }
+
+    
 }
